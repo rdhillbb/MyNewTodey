bfaaa8445ce3a12dc70941fba69772accb7af1a8
Randolph Davis Hill <rdhillbb@icloud.com>
2019-05-02 16:53:02 +0800

    Lastest update

--- MyNewTodey/AppDelegate.swift
+++ MyNewTodey/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -21,29 +21,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         return true
     }
-     //THis Function is called when some external event happens such sms, phone call popup, etc
-    func applicationWillResignActive(_ application: UIApplication) {
-        print("applicationDidEnterBackGround")
-      
-    }
     
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
     func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate")
-        //Save Data prior to Terminating
+        
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+           
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 
--- MyNewTodey/Data Model/ItemKeep.swift
+++ MyNewTodey/Data Model/ItemKeep.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-class Item: Codable {
+class ItemKeep: Codable {
     var title : String = ""
     var done : Bool = false
 }
