3448222e9729963df9098d16e06249b510244a64
Randolph Davis Hill <rdhillbb@icloud.com>
2019-05-14 16:07:06 +0530

    Realm additions added. Completed

--- MyNewTodey/Controllers/TodoListViewController.swift
+++ MyNewTodey/Controllers/TodoListViewController.swift
@@ -89,6 +89,7 @@ class TodoListViewController: UITableViewController {
                     try self.realm.write {
                     let newItem = Item()
                     newItem.title = textField.text!
+                    newItem.dateCreated = Date()
                     currentCategory.items.append(newItem)
                 }
                 } catch {
@@ -163,25 +164,26 @@ class TodoListViewController: UITableViewController {
     
 }
 
-////MARK: - Search Bar Methods
-//extension TodoListViewController: UISearchBarDelegate{
-//
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        let request :  NSFetchRequest<Item> = Item.fetchRequest()
-//        let predicate = NSPredicate(format: "title CONTAINS[cd] %@",searchBar.text!)
-//        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-//        loadItems(with: request,predicate: predicate)
-//
-//    }
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        if searchBar.text?.count == 0 {
-//            loadItems()
-//            DispatchQueue.main.async {
-//                searchBar.resignFirstResponder()
-//            }
-//
-//        }
-//
-//    }
+//MARK: - Search Bar Methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    
+    todoItems = todoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+    
+    tableView.reloadData()
+    
+}
 
-//}
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+          loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+
+        }
+
+    }
+
+}
